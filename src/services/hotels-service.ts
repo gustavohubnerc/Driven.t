ccdad5694de889c2629ba864c6e3e2b18fe487ee
gustavohubnerc <hubnergc@gmail.com>
2023-09-28 13:04:46 -0300

    error treatment

--- src/services/hotels-service.ts
+++ src/services/hotels-service.ts
@@ -1,23 +1,30 @@
-import { notFoundError } from "@/errors";
+import { notFoundError, requestError } from "@/errors";
 import { hotelsRepository } from "@/repositories/hotels-repository";
 
 async function listHotels(userId: number){
     const userEnrollment = await hotelsRepository.getUserEnrollment(userId);
-
     if (!userEnrollment) throw notFoundError();
+    if (!userEnrollment.Ticket.TicketType.includesHotel) throw requestError(402, 'Ticket does not include hotel');
+    if (userEnrollment.Ticket.status !== 'PAID') throw requestError(402, 'Ticket is not paid');
+    if (userEnrollment.Ticket.TicketType.isRemote) throw requestError(402, 'Ticket is remote');
 
     const hotels = await hotelsRepository.listHotels();
+    if (!hotels) throw notFoundError();
 
     return hotels;
 }
 
 async function getHotelRooms(userId: number, hotelId: number) {
     const userEnrollment = await hotelsRepository.getUserEnrollment(userId);
-
     if (!userEnrollment) throw notFoundError();
+    if (!userEnrollment.Ticket.TicketType.includesHotel) throw requestError(402, 'Ticket does not include hotel');
+    if (userEnrollment.Ticket.status !== 'PAID') throw requestError(402, 'Ticket is not paid');
+    if (userEnrollment.Ticket.TicketType.isRemote) throw requestError(402, 'Ticket is remote');
+
+    const hotels = await hotelsRepository.listHotels();
+    if (!hotels) throw notFoundError();
 
     const hotelRooms = await hotelsRepository.getHotelRooms(hotelId);
-
     if (!hotelRooms) throw notFoundError();
 
     return hotelRooms;
