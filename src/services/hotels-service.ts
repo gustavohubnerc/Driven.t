a2fbeef917df44f7ac5c15f6fb5b94c4bac5b514
gustavohubnerc <hubnergc@gmail.com>
2023-09-29 11:18:57 -0300

    test error

--- src/services/hotels-service.ts
+++ src/services/hotels-service.ts
@@ -3,41 +3,73 @@ import { hotelsRepository } from "@/repositories/hotels-repository";
 
 async function listHotels(userId: number){
     const userEnrollment = await hotelsRepository.getUserEnrollment(userId);
-    if (!userEnrollment) throw notFoundError();
+    
+    if (!userEnrollment) {
+        throw notFoundError();
+    }
+
+    if (!userEnrollment.Ticket) {
+        throw requestError(402, 'Ticket not found');
+    }
 
     const paidTicket = userEnrollment.Ticket.status;
-    if (paidTicket !== 'PAID') throw requestError(402, 'Ticket is not paid');
-
     const includesHotel = userEnrollment.Ticket.TicketType.includesHotel;
-    if (includesHotel === false) throw requestError(402, 'Ticket does not include hotel');
-
     const isRemote = userEnrollment.Ticket.TicketType.isRemote;
-    if (isRemote === true) throw requestError(402, 'Ticket is remote');
+
+    if (paidTicket !== 'PAID') {
+        throw requestError(402, 'Ticket is not paid');
+    }
+
+    if (isRemote) {
+        throw requestError(402, 'Ticket is remote');
+    }
+
+    if (!includesHotel) {
+        throw requestError(402, 'Ticket does not include hotel');
+    }
 
     const hotels = await hotelsRepository.listHotels();
-    if (!hotels) throw notFoundError();
+
+    if (!hotels || hotels.length === 0) {
+        throw notFoundError();
+    }
 
     return hotels;
 }
 
+
 async function getHotelRooms(userId: number, hotelId: number) {
     const userEnrollment = await hotelsRepository.getUserEnrollment(userId);
-    if (!userEnrollment) throw notFoundError();
+    
+    if (!userEnrollment) {
+        throw notFoundError();
+    }
+
+    if (!userEnrollment.Ticket) {
+        throw requestError(402, 'Ticket not found');
+    }
 
     const paidTicket = userEnrollment.Ticket.status;
-    if (paidTicket !== 'PAID') throw requestError(402, 'Ticket is not paid');
-
     const includesHotel = userEnrollment.Ticket.TicketType.includesHotel;
-    if (includesHotel === false) throw requestError(402, 'Ticket does not include hotel');
-
     const isRemote = userEnrollment.Ticket.TicketType.isRemote;
-    if (isRemote === true) throw requestError(402, 'Ticket is remote');
-    
-    const hotels = await hotelsRepository.listHotels();
-    if (!hotels) throw notFoundError();
+
+    if (paidTicket !== 'PAID') {
+        throw requestError(402, 'Ticket is not paid');
+    }
+
+    if (isRemote) {
+        throw requestError(402, 'Ticket is remote');
+    }
+
+    if (!includesHotel) {
+        throw requestError(402, 'Ticket does not include hotel');
+    }
 
     const hotelRooms = await hotelsRepository.getHotelRooms(hotelId);
-    if (!hotelRooms) throw notFoundError();
+
+    if (!hotelRooms) {
+        throw notFoundError();
+    }
 
     return hotelRooms;
 }
