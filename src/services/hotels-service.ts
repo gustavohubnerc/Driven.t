866c3097160f1f602b8b02c2ad91938c2c0501bf
gustavohubnerc <hubnergc@gmail.com>
2023-09-29 10:43:05 -0300

    errors test

--- src/services/hotels-service.ts
+++ src/services/hotels-service.ts
@@ -5,14 +5,14 @@ async function listHotels(userId: number){
     const userEnrollment = await hotelsRepository.getUserEnrollment(userId);
     if (!userEnrollment) throw notFoundError();
 
-    const paidTicket = userEnrollment.Ticket.status === 'PAID';
-    if (!paidTicket) throw requestError(402, 'Ticket is not paid');
+    const paidTicket = userEnrollment.Ticket.status;
+    if (paidTicket !== 'PAID') throw requestError(402, 'Ticket is not paid');
 
     const includesHotel = userEnrollment.Ticket.TicketType.includesHotel;
-    if (!includesHotel) throw requestError(402, 'Ticket does not include hotel');
+    if (includesHotel === false) throw requestError(402, 'Ticket does not include hotel');
 
     const isRemote = userEnrollment.Ticket.TicketType.isRemote;
-    if (isRemote) throw requestError(402, 'Ticket is remote');
+    if (isRemote === true) throw requestError(402, 'Ticket is remote');
 
     const hotels = await hotelsRepository.listHotels();
     if (!hotels) throw notFoundError();
